8f3a67a3e09b606e52a01f5668af65aafbcb710f
kalyani9595 <kalyanimano29@gmail.com>
2013-05-12 11:33:49 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -19,6 +19,7 @@ public class ReverseString {
 		while(run == true){
 			System.out.print("Enter a number to choose the method : ");
 			int method = input.nextInt(); 
+/* no default case added */
 
 			switch (method){
 			case 1:
